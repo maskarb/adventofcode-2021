a5cfb0704502e20b510cdd147fae44a58ef80608
Michael Skarbek <mskarbek@redhat.com>
2021-12-11 03:10:07 -0500

    days 10, 11

--- 10/main.py
+++ 10/main.py
@@ -1,15 +1,104 @@
 import os
 import sys
+from collections import defaultdict
 
 def get_input(filename):
     with open(filename, 'r') as f:
-        return f.readlines()
+        return f.read().splitlines()
+
+POINTS = {
+    ")": 3,
+    "]": 57,
+    "}": 1197,
+    ">": 25137,
+}
+AUTOCOMPLETE_POINTS = {
+    ")": 1,
+    "]": 2,
+    "}": 3,
+    ">": 4,
+}
+
+MATCH_PAIR = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+    "<": ">",
+}
+CLOSERS = [")", "]", "}", ">"]
+OPENERS = ["(", "[", "{", "<"]
+MATCHES = ["()", "[]", "{}", "<>"]
 
 def main_p1(filename):
-    pass
+    lines = get_input(filename)
+
+    def recurse(lis, compare, end):
+        if len(lis) <= 2:
+            return
+        if compare in OPENERS:
+            closing = MATCH_PAIR[compare]
+        elif compare in CLOSERS:
+            if compare == end[0]:
+                return recurse(lis[1:], lis[1], end[1:])
+            return compare
+        if lis[1] == closing:
+            return recurse(lis[2:], lis[2], end)
+        elif lis[1] in OPENERS:
+            return recurse(lis[1:], lis[1], closing + end)
+
+        return lis[1]
+
+    invalids = defaultdict(int)
+    for line in lines:
+        invalids[recurse(line, line[0], '')] += 1
+
+    result = 0
+    for k, v in POINTS.items():
+        result += invalids[k] * v
+    print(f"results: {invalids}\n{result}")
+
 
 def main_p2(filename):
-    pass
+    lines = get_input(filename)
+
+    def recurse(lis, compare, end):
+        if len(lis) < 2:
+            # hideous
+            if compare in CLOSERS and compare == end[0]:
+                return end[1:]
+            if compare in OPENERS:
+                closing = MATCH_PAIR[compare]
+                return closing + end
+            return
+        if compare in CLOSERS:
+            if compare == end[0]:
+                return recurse(lis[1:], lis[1], end[1:])
+            return
+        else:
+            closing = MATCH_PAIR[compare]
+            end = closing + end
+            return recurse(lis[1:], lis[1], end)
+
+    autocorrected = []
+    for line in lines:
+        autocorrected.append(recurse(line, line[0], ''))
+
+    results = []
+    for chars in autocorrected:
+        result = 0
+        if not chars:
+            continue
+        for c in chars:
+            result *= 5
+            result += AUTOCOMPLETE_POINTS[c]
+        results.append(result)
+    len_results = len(results)
+    ordered = sorted(results)
+    for i, v in enumerate(ordered):
+        print(i, v)
+    final_result = ordered[int(len_results/2)]
+    print(f"results: {autocorrected}\n{ordered}\n{final_result}")
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 2 or sys.argv[1] not in ('p1', 'p2'):
--- 11/main.py
+++ 11/main.py
@@ -1,15 +1,100 @@
 import os
 import sys
 
+import numpy as np
+
 def get_input(filename):
     with open(filename, 'r') as f:
         return f.readlines()
 
 def main_p1(filename):
-    pass
+    lines = get_input(filename)
+    m = np.array([[int(v) for v in line.strip()] for line in lines]).transpose()
+    x_max, y_max = m.shape
+
+    def increment_value(x, y):
+        if x >= 0 and y >= 0 and x <= x_max - 1 and y <= y_max - 1:
+            m[x, y] += 1
+
+
+    def increment_array(old_tmp):
+        tmp = np.transpose(np.where(m > 9))
+        if tmp.size == 0 and old_tmp.size == 0 or np.array_equal(tmp, old_tmp):
+            return
+        for i in tmp:
+            if i.tolist() in old_tmp.tolist():
+                continue
+            x , y = i
+            p = m[x, y]
+            if p > 9:
+                neighbors = [(x-1, y-1), (x, y-1), (x+1, y-1), (x-1, y), (x+1, y), (x-1, y+1), (x, y+1), (x+1, y+1)]
+                m[x, y] += 1
+                for neighbor in neighbors:
+                    increment_value(*neighbor)
+        increment_array(tmp)
+
+    count = 0
+    for i in range(100):
+        m += 1
+        tmp = np.array([-1, -1])
+        increment_array(tmp)
+
+        for x in range(x_max):
+            for y in range(y_max):
+                p = m[x, y]
+                if p > 9:
+                    count += 1
+                    m[x, y] = 0
+
+    print(m.transpose())
+
+    print(f"result: {count}")
 
 def main_p2(filename):
-    pass
+    lines = get_input(filename)
+    m = np.array([[int(v) for v in line.strip()] for line in lines]).transpose()
+    x_max, y_max = m.shape
+
+    def increment_value(x, y):
+        if x >= 0 and y >= 0 and x <= x_max - 1 and y <= y_max - 1:
+            m[x, y] += 1
+
+
+    def increment_array(old_tmp):
+        tmp = np.transpose(np.where(m > 9))
+        if tmp.size == 0 and old_tmp.size == 0 or np.array_equal(tmp, old_tmp):
+            return
+        for i in tmp:
+            if i.tolist() in old_tmp.tolist():
+                continue
+            x , y = i
+            p = m[x, y]
+            if p > 9:
+                neighbors = [(x-1, y-1), (x, y-1), (x+1, y-1), (x-1, y), (x+1, y), (x-1, y+1), (x, y+1), (x+1, y+1)]
+                m[x, y] += 1
+                for neighbor in neighbors:
+                    increment_value(*neighbor)
+        increment_array(tmp)
+
+    count = 0
+    for i in range(1000):
+        m += 1
+        tmp = np.array([-1, -1])
+        increment_array(tmp)
+
+        for x in range(x_max):
+            for y in range(y_max):
+                p = m[x, y]
+                if p > 9:
+                    count += 1
+                    m[x, y] = 0
+
+        if np.all((m == 0)):
+            break
+
+    print(m.transpose())
+
+    print(f"result: {i+1}")
 
 if __name__ == '__main__':
     if len(sys.argv) < 2 or sys.argv[1] not in ('p1', 'p2'):
